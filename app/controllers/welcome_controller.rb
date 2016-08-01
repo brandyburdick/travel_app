ab32206af34f2fb09b40c3db9b34344fa772aeab
Brandy Burdick <blburdic@asu.edu>
2016-08-01 21:49:34 +0000

    Add Bootstrap, made mobile ready, add Navbar.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @homestate = 'Arizona'
     @countries = ['Italy', 'Scotland', 'Iceland', 'Japan']
-    @images    = ['Iceland_place.jpg', 'Japan_place.jpg', 'Italy_place.jpg']
+    @images    = ['Iceland_place.jpg', 'Japan_place.jpg', 'Italy_place.jpg', 'Scotland_place.jpeg']
   end
 
 
