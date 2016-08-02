76e39898fdfab578b671583febd4e3e45bd7acb3
Brandy Burdick <blburdic@asu.edu>
2016-08-02 18:06:42 +0000

    Add brand name font-family. Add background stylizing. Add table on index page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,10 @@ class WelcomeController < ApplicationController
     @homestate = 'Arizona'
     @countries = ['Italy', 'Scotland', 'Iceland', 'Japan']
     @images    = ['Iceland_place.jpg', 'Japan_place.jpg', 'Italy_place.jpg', 'scotland_image.jpeg']
+    @united_states = {'favorite city'=>'Phoenix', 
+                      'favorite city'=>'Atlanta', 
+                      'favorite state'=>'Washington', 
+                      'flag colors'=>['red', 'green', 'blue', 'orange']}
   end
 
 
